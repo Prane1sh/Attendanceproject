4f419af7caad41917f4d293bcabecce0ca664e6f
Prane1sh <125952875+Prane1sh@users.noreply.github.com>
2023-07-28 11:16:10 +0530

    Add files via upload

---
+++ src/main/java/com/iamneo/security/entity/Role.java
@@ -0,0 +1,9 @@
+package com.iamneo.security.entity;
+
+public enum Role {
+   ADMIN,
+   TEACHER,
+   STUDENT;
+    public static final Role DEFAULT_ROLE = ADMIN; // Set STUDENT as the default role
+
+}
